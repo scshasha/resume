0f03a547ed7487791a680485e9af36b6c415d28a
Siv Shasha <shasha.christopher@gmail.com>
2020-04-17 00:13:17 +0200

    intro text animation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,10 +11,32 @@ function initCustomJS() {
   /** print message to console */
   console.log("DOM ready...");
 
+
+  /** ===================== CATEGORY TOGGLE ============================ */
+
   $('div[class^="cat-item-"]').on('click', (event) => {
     const target = event.target;
 
     $('div[class^="cat-item-"]').removeClass('is-active');
     $(target).addClass('is-active');
   });
+
+
+  /** ===================== ANIMATION ============================ */
+
+  const spans = document.querySelectorAll('h1 span');
+
+  spans.forEach(span => span.addEventListener('mouseover', (e) => {
+    span.classList.add('animated', 'rubberBand');
+  }));
+  spans.forEach(span => span.addEventListener('mouseout', (e) => {
+    span.classList.remove('animated', 'rubberBand');
+  }));
+
+
+
+
+
+
+
 }
