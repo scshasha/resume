c71b4ff338e32f1ff115ddcf69786010fc379af1
Siv Shasha <shasha.christopher@gmail.com>
2020-04-16 02:04:21 +0200

    filter active class refactor

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -13,11 +13,8 @@ function initCustomJS() {
 
   $('div[class^="cat-item-"]').on('click', (event) => {
     const target = event.target;
-    console.log(target);
+
     $('div[class^="cat-item-"]').removeClass('is-active');
     $(target).addClass('is-active');
-    console.log(target);
-
-    // $(this).addClass("is-active").siblings().removeClass("is-active");
   });
 }
