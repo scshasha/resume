9e7de1258fde1333786851485b27a57b766bea61
Siv Shasha <shasha.christopher@gmail.com>
2020-04-18 22:45:28 +0200

    created user service

--- src/app/shared/_services/user.service.ts
+++ src/app/shared/_services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {environment} from "../../../environments/environment";
-import {User} from "../_models/user.model";
+import {environment} from '../../../environments/environment';
+import {User} from '../_models/user.model';
 
 @Injectable({
   providedIn: 'root'
