1bfdccc5733602761daffead7a551b83427cc647
Siv Shasha <shasha.christopher@gmail.com>
2020-04-18 22:43:49 +0200

    moved header and footer components to shared folder

--- src/app/shared/_components/footer/footer.component.ts
+++ src/app/shared/_components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today: any = new Date();
+  linkedinUrl: string = environment.profile_url_linkedin;
+  githubUrl: string = environment.profile_url_github;
+  stackoverflowUrl: string = environment.profile_url_stackoverflow;
+  resumeFile: string = environment.profile_resume;
 
   constructor() { }
 
