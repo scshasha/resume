cd869070fa01d83f206491ae711787144e52a4c6
Siv Shasha <shasha.christopher@gmail.com>
2020-04-18 20:45:22 +0200

    updated environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,8 @@ export const environment = {
   mail_to: 'shaha.christopher@gmail.com',
   mail_subject: 'Howdy, Chris.',
   google_au: '',
+  profile_url_linkedin: 'https://linkedin.com/in/scshasha',
+  profile_url_github: 'https://github.com/scshasha',
+  profile_url_stackoverflow: 'https://stackoverflow.com/users/6050028/iamsiv',
+  profile_resume: 'assets/uploads/files/scshasha-gp_compressed.pdf',
 };
