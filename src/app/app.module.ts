6c84c83165d0e76b63bd23f7be56c31b30e50c35
Siv Shasha <shasha.christopher@gmail.com>
2020-04-18 23:00:22 +0200

    renamed page to admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { FourohfourComponent } from './pages/fourohfour/fourohfour.component';
 import { AlertComponent } from './shared/_components/alert/alert.component';
 import { LoginComponent } from './login/login.component';
 import { ComingsoonComponent } from './pages/comingsoon/comingsoon.component';
-import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
