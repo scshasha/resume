f7a7c1ed21db278d0c805298677742ced3b8ef49
Siv Shasha <shasha.christopher@gmail.com>
2020-05-01 23:56:12 +0200

    using a service to list proects

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,75 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-import { Project } from '../shared/_models/project.model';
-import {ProjectsService} from "../shared/_services/projects.service";
+import {ProjectsService} from '../shared/_services/projects.service';
 
 @Component({
-  selector: 'app-work',
-  templateUrl: './work.component.html',
-  styleUrls: ['./work.component.scss']
+    selector: 'app-work',
+    templateUrl: './work.component.html',
+    styleUrls: ['./work.component.scss']
 })
 export class WorkComponent implements OnInit {
-  public projects = [];
-  public filterData = [];
-  public readonly categories: any = [
-    'All', 'Drupal', 'WordPress', 'Angular', '.Net', 'Adobe'
-  ];
+    public projects = [];
+    public filterData = [];
+    public readonly categories: any = [
+        'All', 'Drupal', 'WordPress', 'Angular', '.Net', 'Adobe'
+    ];
 
-  constructor(
-    private projectsService: ProjectsService
-  ) {
-    this.loadProjects();
-    this.filterData = this.randomizeData(this.projects);
-    console.log(this.filterData);
-  }
+    constructor(
+        private projectsService: ProjectsService
+    ) {
+        this.loadProjects();
+        this.filterData = this.randomizeData(this.projects);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  loadProjects() {
-    this.projects = this.projectsService.get();
+    loadProjects() {
+        this.projects = this.projectsService.get();
     // @todo Subscribe to service.
-  }
+    }
 
-  /**
+    /**
    * filterByCategory
    * Filters projects and returns those that match the selection.
    *
    * @param id
    * @return void|
    */
-  filterByCategory(id: number): void {
-    const tempArray = [];
+    filterByCategory(id: number): void {
+        const tempArray = [];
 
-    // tslint:disable-next-line:triple-equals
-    this.filterData.forEach((project) => {
-      if (project.category == this.categories[id]) {
-        tempArray.push(project);
-      }
-    });
+        // tslint:disable-next-line:triple-equals
+        this.filterData.forEach((project) => {
+            if (project.category === this.categories[id]) {
+                tempArray.push(project);
+            }
+        });
 
-    this.projects = [];
+        this.projects = [];
 
-    if (id === 0) {
-      this.projects = this.filterData;
-    } else {
-      this.projects = this.randomizeData(tempArray);
+        if (id === 0) {
+            this.projects = this.filterData;
+        } else {
+            this.projects = this.randomizeData(tempArray);
+        }
+
     }
 
-  }
-
-  /**
+    /**
    * randomizeData
    * Randomly shuffles data
    *
    * @param data
    * @return array
    */
-  randomizeData(data: any) {
-    for (let i = data.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [data[i], data[j]] = [data[j], data[i]];
+    randomizeData(data: any) {
+        for (let i = data.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [data[i], data[j]] = [data[j], data[i]];
+        }
+        return data;
     }
-    return data;
-  }
 
 }
