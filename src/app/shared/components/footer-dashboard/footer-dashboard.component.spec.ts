55fb4bff2a3a4b430863e7e2484fe5422fbe58c9
Sivuyile Shasha <sivuyile.shasha@wundermanthompson.com>
2022-06-21 09:43:37 +0200

    upgraded to @angular/core@11

--- src/app/shared/components/footer-dashboard/footer-dashboard.component.spec.ts
+++ src/app/shared/components/footer-dashboard/footer-dashboard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FooterDashboardComponent } from './footer-dashboard.component';
 
@@ -6,7 +6,7 @@ describe('FooterDashboardComponent', () => {
   let component: FooterDashboardComponent;
   let fixture: ComponentFixture<FooterDashboardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FooterDashboardComponent ]
     })
--- src/app/shared/components/header-dashboard/header-dashboard.component.spec.ts
+++ src/app/shared/components/header-dashboard/header-dashboard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HeaderDashboardComponent } from './header-dashboard.component';
 
@@ -6,7 +6,7 @@ describe('HeaderDashboardComponent', () => {
   let component: HeaderDashboardComponent;
   let fixture: ComponentFixture<HeaderDashboardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeaderDashboardComponent ]
     })
